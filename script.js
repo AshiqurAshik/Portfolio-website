a8cbcbad63f5faf88874c7bae207708d44586ca9
Ashiqur Rahman <rashiqur688@gmail.com>
2025-04-27 22:14:47 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let link = document.querySelector('.links');
+
+
+function show()
+{
+  if(link.style.display === "none")
+  {
+    link.style.display = "flex";
+  }
+
+  else{
+    link.style.display = "none";
+  }
+}
